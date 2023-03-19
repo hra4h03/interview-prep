0be7d57cd88963222c37bdd5dc3de98b0fd3c481
kiriti <support@rootscope.in>
2023-03-19 11:33:10 +0530

    frontend

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,6 +1,18 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+// import './styles.scss';
+import "../styles/bootstrap.min.css";
+import "../styles/animate.min.css";
+import "../styles/boxicons.min.css";
+import "../styles/meanmenu.min.css";
+import "../styles/flaticon.css";
+// import "../node_modules/react-modal-video/css/modal-video.min.css";
+// import "react-accessible-accordion/dist/fancy-example.css";
+// import "react-tabs/style/react-tabs.css";
+// import "react-image-lightbox/style.css";
+// import "swiper/css/bundle";
+import "../styles/style.css";
+import "../styles/responsive.css";
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
