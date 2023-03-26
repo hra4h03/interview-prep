f9db0925795d05d769c354a4e7ef97d5ddeddef1
kiriti <support@rootscope.in>
2023-03-26 13:24:29 +0530

    add docker compose file

--- apps/backend/src/constants/index.ts
+++ apps/backend/src/constants/index.ts
@@ -1,3 +1,3 @@
 export const CATEGORIES = ['misc', 'javascript', 'bash', 'sequelize', 'pc', 'aws', 'react', 'html', 'css', 'nodejs', 'mongo', 'sql', 'git', 'jenkins', 'cicd', 'bitbucket', 'python', 'expressjs'];
 
-export const DB_URL = `mongodb://localhost:27017`;
+export const DB_URL = `mongodb://0.0.0.0:27017`;
